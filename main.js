5a5fcca1d7987bbc381d0a63b109dc3e48ceae86
Arif F. Khan <faheem49@gmail.com>
2023-11-19 14:01:33 -0500

    updated files

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ cognitoidentityserviceprovider.getUser(params, function(err, data) {
 
         for(var i = 0; i < data.UserAttributes.length; i++)
         {
-            if(data.UserAttributes[i].Name == 'name')
+            if(data.UserAttributes[i].Name == 'Username')
             {
                 UserName = data.UserAttributes[i].Value;
             }
